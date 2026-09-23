c18f80370b901769475a197a530db0337f5cad7d
meottable <slrlalhh1@gmail.com>
2026-09-24 08:57:46 +0900

    Connect verified photo reviews and owner moderation; remove consultation demo and grade claim banners

---
+++ reviews-config.js
@@ -0,0 +1,4 @@
+window.MEOT_REVIEWS = Object.freeze({
+ publicUrl: "https://script.google.com/macros/s/AKfycbz-TSYXiJLUvyMttVjntKMiPh092YIujqQLRgM6jX-ZoZ_qrnyiAfKgez8NSwHkCvzjGw/exec",
+ adminUrl: "https://script.google.com/macros/s/AKfycbzK_oFfTpKLZVCsrheLn8leiVrO9PsGGVZaF6TtaOlArlveX3P3nZ1gopQ2PybM-FxL-g/exec"
+});
